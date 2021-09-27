f9485b7dc224a6d5ab12706aaedbe323fd62d940
conant2 <61479615+conant2@users.noreply.github.com>
2021-09-27 12:39:28 -0400

    deleted for loop

--- bot.py
+++ bot.py
@@ -11,13 +11,9 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    for guild in client.guilds:
-        if guild.name == GUILD:
-            break
-
     print(
         f'{client.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})'
+        f'did this work?{client.guilds}'
     )
 
 

30aa4cb3b278d59331b9e49b3d8926b90e9c1a3d
Tony K <82545368+Nsiem@users.noreply.github.com>
2021-10-28 18:45:26 -0400

    made discord bot capable of searching anime and retrieving image and displaying in chat

--- src/bot.py
+++ src/bot.py
@@ -1,6 +1,11 @@
+from asyncio.windows_events import NULL
 import os
 import random
 import discord
+from PIL import Image
+import PIL
+from io import BytesIO
+from test import get_anime_list, get_anime_image
 
 
 from discord.ext import commands
@@ -33,6 +38,17 @@ async def create_channel(ctx, channel_name='real-python'):
     if not existing_channel:
         print(f'Creating a new channel: {channel_name}')
         await guild.create_text_channel(channel_name)
+
+@bot.command(name="searchAnime")
+async def searchanime(ctx, *, search_query: str):
+    aniquery = search_query.replace(" ", "%20")
+    anime_list = await get_anime_list(aniquery, NULL)
+    anime_img = await get_anime_image(anime_list['data'][0]['node']['main_picture']['medium'])
+    anime_img.save('temp.jpg')
+
+    title = anime_list['data'][0]['node']['title']
+    await ctx.send(title, file=discord.File('temp.jpg'))
+    os.remove('temp.jpg')
 
 @bot.event
 async def on_command_error(ctx, error):
--- src/test.py
+++ src/test.py
@@ -2,30 +2,34 @@ from asyncio.windows_events import NULL
 import aiohttp
 import asyncio
 import json
+from PIL import Image
+from io import BytesIO
 
 with open('token.json') as t:
     tokens = json.load(t)
 
 access_token = tokens['access_token']
 refresh_token = tokens['refresh_token']
+header = {"Authorization": f'Bearer {access_token}'}
 
 async def main():
     searchrequest = input("Please enter the title you wish to search: " )
-    anime_list = await get_anime_list(searchrequest, NULL)
-    print(anime_list['data'])
-    while(1):
-        nextsearchflag = input("Please enter 1 to see next page or 2 if you found what you are looking for: ")
-        if(nextsearchflag == '2'):
-            break
-        else:
-            print(anime_list['paging']['next'])
-            anime_list = await get_anime_list(NULL, anime_list['paging']['next'])
-            print(anime_list['data'])
+    anime_img = await get_anime_image('https://api-cdn.myanimelist.net/images/anime/1412/107914.jpg')
+    anime_img.show()
+    # anime_list = await get_anime_list(searchrequest, NULL)
+    # print(anime_list['data'])
+    # while(1):
+    #     nextsearchflag = input("Please enter 1 to see next page or 2 if you found what you are looking for: ")
+    #     if(nextsearchflag == '2'):
+    #         break
+    #     else:
+    #         print(anime_list['paging']['next'])
+    #         anime_list = await get_anime_list(NULL, anime_list['paging']['next'])
+    #         print(anime_list['data'])
     
 
 async def get_anime_list(searchQuery: str, customurl: str) -> json:
-    url = f'https://api.myanimelist.net/v2/anime?q={searchQuery}&limit=10'
-    header = {"Authorization": f'Bearer {access_token}'}
+    url = f'https://api.myanimelist.net/v2/anime?q={searchQuery}&limit=5'
     async with aiohttp.ClientSession(headers=header) as session:
         if (customurl != NULL):
             url = customurl
@@ -33,6 +37,12 @@ async def get_anime_list(searchQuery: str, customurl: str) -> json:
             anime_list = await resp.json()
     session.close
     return anime_list
+
+async def get_anime_image(url: str):
+    async with aiohttp.ClientSession(headers=header) as session:
+        async with session.get(url) as resp:
+            anime_img = Image.open(BytesIO(await resp.read()))
+            return anime_img
 
 
 if __name__ == '__main__':
